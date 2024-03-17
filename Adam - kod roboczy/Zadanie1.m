277b0a73511c430791aad3aec51f1e3255dcb645
Turbobolekus <adam01wroblewski@gmail.com>
2024-03-17 16:38:31 +0100

    porównanie ciągły zlinearyzowany - ciągły nieliniowy

--- Adam - kod roboczy/Zadanie1.m
+++ Adam - kod roboczy/Zadanie1.m
@@ -1,5 +1,3 @@
-clear
-clc
 % stałe
 global Fh_in Fc_in Fd_in Th Tc Td alpha r tau_c tau_h Fd h_pp T_pp
 r = 68;
@@ -16,33 +14,36 @@ tau_h = 220;
 h_pp = 13.5424;
 T_pp = 38.0978;
 
+% [6 13 20 27 44 51 58];
+for i = [6 13 20 27 34 41 48]
+    Fc_in(1:400) = 50;
+    Fh_in(1:400) = 27;
+    
+    % skok wartości
+    Ts = 16000;
+    Fc_in(401:Ts) = 50;
+    Fh_in(401:Ts) = i;
+    
+    
+    [h, T, t] = obiekt_ciagly(0, Ts, h_pp, T_pp);
+    [h_zlin, T_zlin, t_zlin] = obiekt_ciagly(1, Ts, h_pp, T_pp);
+    
+    
+    % wykresy ciagly
+    figure(1)
+    grid on
+    plot(t,h, 'color','red');
+    hold on
+    plot(t_zlin,h_zlin, 'color','blue');
+    title('ciągłe');
+    legend('h', 'h_zlin');
+end
 
-Fc_in(1:400) = 50;
-Fh_in(1:400) = 27;
-
-% skok wartości
-Ts = 16000;
-Fc_in(401:Ts) = 50;
-Fh_in(401:Ts) = 27;
-
-
-[h, T, t] = obiekt_ciagly(0, Ts, h_pp, T_pp);
-% [h_zlin, T_zlin, t_zlin] = obiekt_ciagly(1, Ts, h_pp, T_pp);
-
-
-% % wykresy ciagly
-figure(1)
-grid on
-plot(t,h);
-hold on
-title('ciągłe');
-legend('h');
-
-figure(2)
-grid on
-plot(t,T);
-title('Nieliniowe');
-legend('T');
+% figure(2)
+% grid on
+% plot(t,T);
+% title('Nieliniowe');
+% legend('T');
 % 
 % figure(3)
 % grid on
