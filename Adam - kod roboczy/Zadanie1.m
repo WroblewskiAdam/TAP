e95e9b1f7e148e9d7e42434338532e85bcc3c4cb
Turbobolekus <adam01wroblewski@gmail.com>
2024-03-22 19:48:56 +0100

    linearyzacja - ciąg dalszy spierdolonej trylogii

--- Adam - kod roboczy/Zadanie1.m
+++ Adam - kod roboczy/Zadanie1.m
@@ -17,50 +17,100 @@ tau_c = 170;
 tau_h = 220;
 h_pp = 13.5424;
 T_pp = 38.0978;
+% 38.0978260869565
+
 Tp = 20;
+
+Ts = 8000;
+Fc_skoki = 20:1:80;
+Fh_skoki = 7:1:47;
+skoki_h = [];
+skoki_T = [];
 
 % [6 13 20 27 34 41 48]
 % [21 23 25 27 29 31 33]
 % [24 25 26 27 28 29 30]
-for i = [24 25 26 27 28 29 30]
+for i = Fh_skoki
     Fc_in(1:400) = 50;
     Fh_in(1:400) = 27;
-    
-    % skok wartości
-    Ts = 14000;
-    Fc_in(401:Ts) = 50;
-    Fh_in(401:Ts) = i;
-    
+  
+
+    % % skok wartości
+    % settle_time = 10000;
+    % Ts = 4*settle_time;
+    % Fh_in(401:settle_time) = 32;
+    % Fh_in(settle_time+1:2*settle_time) = 22;
+    % Fh_in(2*settle_time+1:3*settle_time) = 37;
+    % Fh_in(3*settle_time+1:Ts) = 17;
+
+    Fc_in(401:Ts) = i;
+    Fh_in(401:Ts) = 27;
     
     [h, T, t] = obiekt_ciagly(0, Ts, h_pp, T_pp);
     [h_zlin, T_zlin, t_zlin] = obiekt_ciagly(1, Ts, h_pp, T_pp);
     % [h_d, T_d] = obiekt_dyskretny(Ts/Tp, h_pp, T_pp, Tp);
+
+    skoki_h = [skoki_h, h(end)];
+    skoki_T = [skoki_T, T(end)];
+    
+    % figure(1)
+    % subplot(2,1,1)
+    % plot(t,h, 'color','red');
+    % title('przebieg zmeinnej h dla skoków sterowania Fh');
+    % grid on
+    % legend('h');
     % 
-    % % h = (h - h(1))/7;
-    % figure(11)
+    % subplot(2,1,2)
+    % plot(t,T, 'color','red');
+    % title('przebieg zmeinnej T dla skoków sterowania Fh')
+    % grid on
+    % legend('T');
+    % print("rysunki/skoki sterowania Fh - przebiegi.png","-dpng","-r400")
+    % 
+    % 
+    % figure(2)
+    % subplot(2,1,1)
+    % plot(t,Fc_in, 'Color','blue');
+    % title('Sterowanie Fc');
+    % legend('Fc');
+    % grid on;
+    % 
+    % subplot(2,1,2)
+    % plot(t,Fh_in, 'color','blue');
+    % title('Sterowanie Fh');
+    % grid on
+    % legend('Fh');
+    % print("rysunki/skoki sterowania Fh - sterowanie.png","-dpng","-r400")
+
+
+
+    % h = (h - h(1))/7;
+    % subplot(2,1,1)
     % grid on
     % plot(t,h, 'color','red');
-    % title('h ciągłe');
+    % title('punkt pracy');
     % legend('h');
     % 
     % % T = (T - T(1))/7;
-    % figure(22)
+    % subplot(2,1,2)
     % grid on
     % plot(t,T, 'color','red');
-    % title('T ciągłe');
+    % title('punkt pracy');
     % legend('T');
+    % print("rysunki/pkt_pracy.png","-dpng","-r400")
 
+    
 
-    % wykresy ciagly - zline
-    figure(1);
-    plot(t,h, 'color','red');
-    hold on;
-    plot(t_zlin,h_zlin, 'color','blue');
-    title('ciągłe');
-    legend('h', 'h-zlin');
-    grid on;
-    grid minor;
-
+    % % wykresy ciagly - zline
+    % figure(1);
+    % plot(t,h, 'color','red');
+    % hold on;
+    % plot(t_zlin,h_zlin, 'color','blue');
+    % title('ciągłe');
+    % legend('h', 'h-zlin');
+    % grid on;
+    % grid minor;
+    % 
     % figure(2);
     % plot(t,T, 'color','red');
     % hold on;
@@ -90,3 +140,24 @@ for i = [24 25 26 27 28 29 30]
     % title('porównanie ciągłe zlin - dyskretne zlin dla zmiennej T');
     % legend('ciagly-zlin', 'dyskretny-zlin');
 end
+
+figure(1)
+hold on
+plot(Fh_skoki,skoki_h, 'color','red');
+title('Charakterystyka statyczna toru Fd - h');
+xlabel('Fc')
+ylabel('h')
+grid on;
+print("rysunki/char_stat toru Fd-h.png","-dpng","-r400")
+
+
+figure(2)
+hold on
+plot(Fh_skoki,skoki_T, 'color','red');
+title('Charakterystyka statyczna toru Fd - T');
+xlabel('Fc')
+ylabel('T')
+grid on;
+print("rysunki/char_stat toru Fd-T.png","-dpng","-r400")
+
+
--- Adam - kod roboczy/f_ciagla_zlin.m
+++ Adam - kod roboczy/f_ciagla_zlin.m
@@ -1,5 +1,5 @@
 function [X] = f_ciagla_zlin(t, X)
-	global Fh_in Fc_in Fd_in Th Tc Td alpha r tau_c tau_h Fd h_pp
+	global Fh_in Fc_in Fd_in Th Tc Td alpha r tau_c tau_h Fd h_pp T_pp
 	Fd = 15;
 	h = X(1,:);
     T = X(2,:);
@@ -18,16 +18,25 @@ function [X] = f_ciagla_zlin(t, X)
     end
 
     h0 = h_pp;
+    T0 = T_pp;
     Fh0 = 50;
     Fc0 = 27;
+    
 	% h_ = (1/(2*pi*r*h0 - pi*h0^2) + (2*h0-2*r)/(pi*h0^2*(h0-2*r)^2)*(h-h0)) * (Fh +Fc + Fd - alpha*(sqrt(h0) + 1/(2*sqrt(h0))*(h-h0)));
+
     h_ = (Fh0 +Fc0 + Fd - alpha*sqrt(h0)) / (2*pi*r*h0 - pi*h0^2) + ...
-         ((3*alpha*h0^(3/2) - 2*alpha*sqrt(h0)*r + 4*Fc0*(h0-r) + 4*Fd*(h0-r) + 4*h0*Fh0 - 4*Fh0*r) / (2*pi*h0^2*(h0-2*r)^2)) * (h-h0) + ...
-         (1/(2*pi*h0*r - pi*h0^2))*(Fh - Fh0) + ...
-         (1/(2*pi*h0*r - pi*h0^2))*(Fc - Fc0);
+         ((alpha*sqrt(h0)*(3*h0-2*r) - 4*(h0-r)*(Fc0+Fh0+Fd)) / (2*pi*h0^2*(h0-2*r)^2)) * (h-h0) + ...
+         (1/(2*pi*r*h0 - pi*h0^2))*(Fh - Fh0) + ...
+         (1/(2*pi*r*h0 - pi*h0^2))*(Fc - Fc0);
 
+    % T = (Fh*Th +Fc*Tc + Fd*Td - alpha*sqrt(h)*T) / (((pi*h^2)/3) * (3*r-h));
+    T = (Fh0*Th +Fc0*Tc + Fd*Td - alpha*sqrt(h0)*T) / (((pi*h0^2)/3) * (3*r-h0)) + ...
+        ((3*alpha)/(h0*sqrt(h0)*(pi*h0-3*pi*r))) * (T - T0) - ...
+        ((3*Th)/(pi*h0^2*(h0-3*r))) * (Fh - Fh0) - ...
+        ((3*Tc)/(pi*h0^2*(h0-3*r))) * (Fc - Fc0) + ...
+        ((3*alpha*sqrt(h0)*T0*(9*r-5*h0) + 18*(h0 - 2*r)*(Fc0*Tc + Fh0*Th + Fd*Td)) / (2*pi*h0^3*(h0-3*r)^2)) *(h-h0);
 
-    T = (Fh*Th +Fc*Tc + Fd*Td - alpha*sqrt(h)*T) / (((pi*h^2)/3) * (3*r-h));
+    
     X = [h_; T];
     
 end
