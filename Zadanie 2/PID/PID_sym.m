b9bb75ab100db6e557869439a19720fa115d015f
kubiakm <g.kubiak77@gmail.com>
2024-05-16 04:17:39 +0200

    mam dosc

--- Zadanie 2/PID/PID_sym.m
+++ Zadanie 2/PID/PID_sym.m
@@ -27,36 +27,66 @@ tau_c_steps = round(170 / Tp);
 tau_h_steps = round(220 / Tp);
 
 k_min = 1 + max(tau_c_steps, tau_h_steps);
-k_max = steps;  
+k_max = steps + 22;  
 
 %% bez odsprzegania
 % metoda roznic skonczonych
-% p = [3.2419   -0.0009   -9.8430    10    0.0164    3];
-p = [0.6426    0.0659   -4.9959   -5.0000    1.1975   -5.0000];
+% p = [-3.6741    0.0002   -4.9999    5.0000   -0.0014    4.9999];
 
 
 %% z odsprzeganiem
-% p = [1.7309   -0.0031   -3.9315    7.8976   -0.0069    0.3743];
-% p = [1.7276   -0.0030    8.1169    7.9134   -0.0070    0.3589];
-% p = [3.4059   -0.0037   -9.8133    9.9798   -0.0084    4.9129];
-% p = [3.4846   -0.0039  -19.7988   10.4474   -0.0088    4.6916];
+% p = [-1.8399   -0.0006   -4.9576    4.9826   -0.0053    4.9443];
+% p = [-1.8290   -0.0006   -4.9999    5.0000   -0.0053    4.9999];
 
-[e, T_out, h_out, T_zad, h_zad, Fc_in, Fh_in] = PID(p);
+[e, T_out, h_out, T_zad, h_zad, Fc_in, Fh_in, Fd_in, Td_in] = PID(p);
 
 disp(e)
 
 figure(1)
+subplot(2,1,1); 
 hold on
-plot(T_out(k_min:k_max))
-plot(T_zad(k_min:k_max))
+grid on;
 plot(h_out(k_min:k_max))
 plot(h_zad(k_min:k_max))
-hold off
-legend(["T_{out}", "Tzad", "h_{out}", "hzad"])
+title('Wyjście h');
+xlabel('k');
+ylabel('wyjście'); 
+legend(["h", "h_{zad}"]) 
 
-figure(2)
+
+subplot(2,1,2); 
 hold on
-plot(Fc_in(k_min:k_max))
-plot(Fh_in(k_min:k_max))
-hold off
-legend(["Fc_{in}", "Fh_{in}"])
+grid on;
+plot(T_out(k_min:k_max))
+plot(T_zad(k_min:k_max))
+title('Wyjście T');
+xlabel('k');
+ylabel('wyjście'); 
+legend(["T", "T_{zad}"]) 
+
+figure(2); 
+subplot(4,1,1); 
+plot(Fc_in(k_min:k_max));
+title('Fc'); % 
+ylabel('Sterowanie Fc'); 
+grid on; 
+
+
+subplot(4,1,2); 
+plot(Fh_in(k_min:k_max));
+title('Fh');
+ylabel('Sterowanie Fh');
+grid on;
+
+subplot(4,1,3); 
+plot(Fd_in(k_min:k_max));
+title('Fd');
+ylabel('Zakłócenie Fd');
+grid on;
+
+subplot(4,1,4); % Czwarty subwykres
+plot(Td_in(k_min:k_max));
+title('Td');
+ylabel('Zakłócenie Td');
+xlabel('k'); % Etykieta osi X tylko dla ostatniego wykresu
+grid on;
